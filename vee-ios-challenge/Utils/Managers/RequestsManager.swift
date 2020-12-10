3d3707f5ca3fce4c28806948834d5b63e76c6dab
rajinavarro <rajinavarro09@gmail.com>
2020-12-10 16:54:10 -0300

    RequestManager criado

--- vee-ios-challenge/Utils/Managers/RequestsManager.swift
+++ vee-ios-challenge/Utils/Managers/RequestsManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+class RequestsManager {
+    
+    static let shared = RequestsManager()
+    
+    private init() {}
+    
+    func makeRequest(to url: String, method: RequestMethod, completion: @escaping (Data?, String?) -> Void ) {
+        
+        guard let parsedURL = URL(string: url) else {
+            return
+        }
+        
+        var request = URLRequest(url: parsedURL)
+        let session = URLSession.shared
+        request.httpMethod = method.rawValue
+        
+        
+        session.dataTask(with: request) { (data, response, error) in
+            guard let _ = error else {
+                completion(data, nil)
+                return
+            }
+            completion(nil, error.debugDescription)
+            return
+        }.resume()
+        
+        request.httpMethod = method.rawValue
+    }
+}
