8a8aad7f6ab7762eb012f8f89b17a18f1656ffea
rajinavarro <rajinavarro09@gmail.com>
2020-12-10 16:54:19 -0300

    DateExtensions criado

--- vee-ios-challenge/Utils/Extensions/DateExtensions.swift
+++ vee-ios-challenge/Utils/Extensions/DateExtensions.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+
+extension Date{
+    
+    var currentTimeStamp: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy_MM_dd_hh_mm_ss"
+        return (formatter.string(from: Date()) as NSString) as String
+    }
+}
