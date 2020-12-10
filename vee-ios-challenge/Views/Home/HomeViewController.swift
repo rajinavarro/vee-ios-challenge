906813116473a7314ac7b94cf60b5ec2d947161a
rajinavarro <rajinavarro09@gmail.com>
2020-12-10 16:52:33 -0300

    HomeViewController chamando viewModel

--- vee-ios-challenge/Views/Home/HomeViewController.swift
+++ vee-ios-challenge/Views/Home/HomeViewController.swift
@@ -5,4 +5,33 @@
 //  Created by raji navarro on 10/12/20.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController{
+    
+    
+    lazy var viewModel: HomeViewModel = {
+        let obj = HomeViewModel()
+        obj.delegate = self
+        return obj
+    }()
+    
+    var heroes = [Result]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.getHeroes()
+    }
+}
+
+extension HomeViewController: HomeViewModelDelegate{
+    func finishFetchCharacters(characters: [Result]) {
+        self.heroes.append(contentsOf: characters)
+    }
+    
+    func failFetchingCharacters(errorMessage: String) {
+        print("erro")
+    }
+    
+    
+}
