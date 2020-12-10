551b6fe374491766129e94e42c6bacc003f1bcfb
rajinavarro <rajinavarro09@gmail.com>
2020-12-10 16:52:11 -0300

    Model Character criado

--- vee-ios-challenge/Models/Character.swift
+++ vee-ios-challenge/Models/Character.swift
@@ -5,12 +5,22 @@
 //  Created by raji navarro on 10/12/20.
 //
 
-import Foundation
-
-struct Character: Codable{
-    let id: Int?
-    let name: String?
-    let description: String?
-    let modified: Date?
-    
-}
+//import Foundation
+//
+//struct Character: Codable {
+//    let id: Int
+//    let name, resultDescription: String
+//    let modified: Date
+//    let thumbnail: Thumbnail
+//    let resourceURI: String
+//    let comics, series: Comics
+//    let stories: Stories
+//    let events: Comics
+//    let urls: [URLElement]
+//
+//    enum CodingKeys: String, CodingKey {
+//        case id, name
+//        case resultDescription = "description"
+//        case modified, thumbnail, resourceURI, comics, series, stories, events, urls
+//    }
+//}
